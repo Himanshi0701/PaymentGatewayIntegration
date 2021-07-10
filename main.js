2e1ba81f20f09db4d3c8893d68dc92bdf1bd0694
Himanshi0701 <mahimanshi01@gmail.com>
2021-07-10 18:27:03 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const back2top = document.querySelector("#back2top");
+
+back2top.addEventListener('click', (e)=>{
+    e.preventDefault();
+    window.scroll({top:0, left:0, behavior:"smooth"});
+});
